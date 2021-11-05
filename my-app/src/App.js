e9dbfa9832b43d3e5a9504313ccf7427164797e8
MUHAMMAD-USAMA-ASHRAF <muhammadusamaashrafsam786@gmail.com>
2021-11-05 14:24:51 -0700

    assignment02 done
    HELLO WORLD+NAME,
    NEW CSS FILE MADE

---
+++ my-app/src/App.js
@@ -0,0 +1,36 @@
+import logo from './logo.svg';
+import './App.css';
+import './new.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+          <p></p>
+          <div className = "letsgo">
+       HELLO WORLD FROM MUHAMMAD-USAMA-ASHRAF
+
+    </div>
+    <p></p>
+    <div className = "two">
+           
+       JUST WANTED TO HOST A DEMO REACT APP WITH THE LOGO THE IMAGE
+       </div>
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
